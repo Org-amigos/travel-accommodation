5a40ff69d0f9bf30451763b2d99350b894ffba7e
shaik sajid <shaiksajid492@gmail.com>
2021-07-18 17:41:10 +0530

    1st commit

---
+++ src/main/java/com/travel/TravelAccommodationApplication.java
@@ -0,0 +1,13 @@
+package com.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAccommodationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAccommodationApplication.class, args);
+	}
+
+}
